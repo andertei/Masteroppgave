eae437dba04d9e3fb2406102eb0fb56e94da583b
Anders Dall'Osso Teigset <andertei@ark-shack01.samfundet.no>
2014-01-20 17:57:50 +0000

    Filtrerte data, moset sammen kurvene. Fjernet ~-dritt

--- Matlab/csvReader.m
+++ Matlab/csvReader.m
@@ -7,14 +7,23 @@ test_ONE_OK=load('12_400_d4_d8_Ok_new.lvm');
 
 test_TWO_OK=load('15_400_d4_d8_OK.lvm');
 
-[m,i]=max(test_ONE_OK(:,2));
-[m,j]=max(test_TWO_OK(:,2));
+wnd = 500;output_ONE = filter(ones(wnd, 1)/wnd, 1, test_ONE_OK(:,2));
 
-test_TWO_OK(:,1)=test_TWO_OK(:,1)+(test_ONE_OK(i,1)-test_TWO_OK(j,1));
+wnd = 500;output_TWO = filter(ones(wnd, 1)/wnd, 1, test_TWO_OK(:,2)); %smoother dataene
 
-plot(test_ONE_OK(:,1),test_ONE_OK(:,2),'r');
+diffV1=diff(output_ONE); %finner de deriverte
+diffV2=diff(output_TWO);
+
+[m,i]=max(diffV1); %finner posisjonen til max av de deriverte
+[m,j]=max(diffV2);
+
+test_TWO_OK(:,1)=test_TWO_OK(:,1)+(test_ONE_OK(i,1)-test_TWO_OK(j,1)); %tidsforskyver
+
+plot(test_ONE_OK(:,1),output_ONE,'r');
 
 hold on
 
-plot(test_TWO_OK(:,1),test_TWO_OK(:,2),'b');
+plot(test_TWO_OK(:,1),output_TWO,'b');
+
+%http://www.mathworks.se/help/matlab/creating_plots/using-multiple-x-and-y-axes.html
 
